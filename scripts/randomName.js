a688bda09be03af5d4ed2e5d99638a3a831a6424
CredibleOpossum <60998830+CredibleOpossum@users.noreply.github.com>
2020-08-19 21:28:16 -0400

    Making code look nice, fixing typo.

--- scripts/randomName.js
+++ scripts/randomName.js
@@ -1,10 +1,8 @@
 var first = ["Mystical", "Complicated", "Abstract", "Complex", "Premium", "Scared", "Joyous", "Festive", "Selective", "Werid"];
 
-var secound = ["Vase", "Cat", "Quiver", "Idea", "Lamp", "Haircut", "Partner", "Cover", "Rain", "Lace"];
+var second = ["Vase", "Cat", "Quiver", "Idea", "Lamp", "Haircut", "Partner", "Cover", "Rain", "Lace"];
 
 function getName() {
-	name = "";
-	name += first[randomInt(0, first.length - 1)];
-	name += " " + secound[randomInt(0, secound.length - 1)];
+	name = `${first[randomInt(0, first.length - 1)]} ${second[randomInt(0, second.length - 1)]}`;
 	return name;
 }
