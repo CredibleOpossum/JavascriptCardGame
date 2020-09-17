1419740da7b4e7daace87ab47e191a95b9937416
Zander <60998830+CredibleOpossum@users.noreply.github.com>
2020-09-16 20:20:13 -0400

    Use tabs, unsure why this was spaces.

--- scripts/save.js
+++ scripts/save.js
@@ -1,39 +1,39 @@
 if (localStorage.save) {
-  game = JSON.parse(atob(localStorage.save))
-  for (card in game.equippedCards) {
-    addCard("equippedCards", card)
-  }
-  for (card in game.cards) {
-    addCard("cardList", card)
-  }
-  document.getElementById("maxCards").innerText = `Equipped cards (${game.maxEquippedCards} max):`
+	game = JSON.parse(atob(localStorage.save));
+	for (card in game.equippedCards) {
+		addCard("equippedCards", card);
+	}
+	for (card in game.cards) {
+		addCard("cardList", card);
+	}
+	document.getElementById("maxCards").innerText = `Equipped cards (${game.maxEquippedCards} max):`;
 } else {
-  game = {
-    money: 0,
-    maxEquippedCards: 1,
-    luck: 0,
-    cards: {},
-    equippedCards: {},
-    upgradeCosts: {
-      maxEquippedCards: 100
-    },
-    upgrades: {
-      cardAmount: {
-        count: 0,
-        cost: 100,
-        scale: 10
-      },
-      luck: {
-        count: 0,
-        cost: 1000,
-        scale: 2
-      }
-    }
-  }
+	game = {
+		money: 0,
+		maxEquippedCards: 1,
+		luck: 0,
+		cards: {},
+		equippedCards: {},
+		upgradeCosts: {
+			maxEquippedCards: 100,
+		},
+		upgrades: {
+			cardAmount: {
+				count: 0,
+				cost: 100,
+				scale: 10,
+			},
+			luck: {
+				count: 0,
+				cost: 1000,
+				scale: 2,
+			},
+		},
+	};
 }
 
 function save() {
-  localStorage.save = btoa(JSON.stringify(game));
+	localStorage.save = btoa(JSON.stringify(game));
 }
 
-setInterval(save, 2000)
+setInterval(save, 2000);
